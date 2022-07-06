0ab6bbf113f1a08eaceec19b38505266b2cd5895
jhu1 <jiahu@i9i8.com>
2022-07-06 17:36:22 +0800

    monitoring

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rootPath=$(cd "$(dirname "$0")";pwd)
-GOOS=linux GOARCH=amd64 go build -o ${rootPath}/bin/main ${rootPath}/main.go
+GOOS=linux GOARCH=amd64 go build -o ${rootPath}/bin/monitoring ${rootPath}/main.go
 #GOOS=darwin GOARCH=amd64 go build -o ${rootPath}/bin/main ${rootPath}/main.go
