5b0418430faa2257c0c61b5a0436d992c36e1bb7
jhu1 <jiahu@i9i8.com>
2022-07-06 15:52:20 +0800

    pay success

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 rootPath=$(cd "$(dirname "$0")";pwd)
-GOOS=darwin GOARCH=amd64 go build -o ${rootPath}/bin/main ${rootPath}/main.go
+GOOS=linux GOARCH=amd64 go build -o ${rootPath}/bin/main ${rootPath}/main.go
+#GOOS=darwin GOARCH=amd64 go build -o ${rootPath}/bin/main ${rootPath}/main.go
