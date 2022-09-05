01da192745859821b99b098c4113cdf5e32face6
jhu1 <jiahu@i9i8.com>
2022-09-05 17:30:19 +0800

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rootPath=$(cd "$(dirname "$0")";pwd)
+go mod tidy
 GOOS=linux GOARCH=amd64 go build -o ${rootPath}/bin/monitoring ${rootPath}/main.go
 #GOOS=darwin GOARCH=amd64 go build -o ${rootPath}/bin/main ${rootPath}/main.go
