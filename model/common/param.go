0c8133727502fbd39678f937b4d80732abdf54eb
jhu1 <jiahu@i9i8.com>
2022-10-18 17:41:17 +0800

    监控优化

--- model/common/param.go
+++ model/common/param.go
@@ -1,9 +1,10 @@
 package common
 
 type Param struct {
-	RootPath       *string `json:"rootPath"`
-	ConfigFileName *string `json:"configFileName"`
-	ModuleName     *string `json:"moduleName"`
-	Debug          *bool   `json:"debug"`
-	Port           *uint64 `json:"port"`
+    RootPath       *string `json:"rootPath"`
+    ConfigFileName *string `json:"configFileName"`
+    ModuleName     *string `json:"moduleName"`
+    MonitorName    *string `json:"monitorName"`
+    Debug          *bool   `json:"debug"`
+    Port           *uint64 `json:"port"`
 }
