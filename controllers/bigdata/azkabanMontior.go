4f1551eca37e2086db950abca3d9adc8e20bd583
bobzhu <bob.zhu@abunistyle.com>
2022-12-02 15:37:47 +0800

    更改sql逻辑为 查询上一小时失败或者killed任务

--- controllers/bigdata/azkabanMontior.go
+++ controllers/bigdata/azkabanMontior.go
@@ -44,9 +44,13 @@ func (a *AzkabanMotior) Sendnotice() {
 
 	sql := "select flow_id,status " +
 		"from azkaban.execution_jobs " +
-		"where DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 7 hour),'%Y-%m-%d %H')<=? " +
-		"and DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 8 hour),'%Y-%m-%d %H')>=? " +
-		"and status in (70,60)"
+		"where DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 8 hour),'%Y-%m-%d %H') = DATE_FORMAT(date_add(?,interval -1 hour),'%Y-%m-%d %H') " +
+		"and status in (70,60)" +
+		"and flow_id not in(" +
+		"select flow_id " +
+		"from azkaban.execution_jobs " +
+		"where DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 8 hour),'%Y-%m-%d %H') = DATE_FORMAT(date_add(?,interval -1 hour),'%Y-%m-%d %H') " +
+		"and status not in (70,60))"
 	rows, err := db.Query(sql, currentTime, currentTime)
 
 	if err != nil {
@@ -66,12 +70,14 @@ func (a *AzkabanMotior) Sendnotice() {
 		if executionJobs.status == 70 {
 			message := "\nazkaban调度" + "\nazkaban任务：" + executionJobs.flowJob + "\n任务状态：failed"
 			fmt.Println(message)
-			resp, err := http.Get(fmt.Sprintf("http://voice.arch800.com/notice/singleCallByTts?system=Monitoring&errorMsg=%s", url.QueryEscape(message)))
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(resp)
+			/*
+				resp, err := http.Get(fmt.Sprintf("http://voice.arch800.com/notice/singleCallByTts?system=Monitoring&errorMsg=%s", url.QueryEscape(message)))
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println(resp)
+			*/
 		}
 
 		if executionJobs.status == 60 {
