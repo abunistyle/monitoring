055435db0df41da48d05c3988c3f7b057884a1ac
bobzhu <bob.zhu@abunistyle.com>
2022-12-05 11:38:47 +0800

    更改sql逻辑为 查询最新一条任务状态

--- controllers/bigdata/azkabanMontior.go
+++ controllers/bigdata/azkabanMontior.go
@@ -42,15 +42,18 @@ func (a *AzkabanMotior) Sendnotice() {
 	currentTime := time.Now().Format("2006-01-02 15")
 	fmt.Println(currentTime)
 
-	sql := "select flow_id,status " +
+	sql := "select a.flow_id,a.status " +
+		"from (" +
+		"select flow_id,status,end_time " +
+		"from azkaban.execution_jobs " +
+		"where DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 8 hour),'%Y-%m-%d %H') = DATE_FORMAT(date_add(?,interval -1 hour),'%Y-%m-%d %H'))a, " +
+		"( " +
+		"select flow_id,max(end_time) end_time " +
 		"from azkaban.execution_jobs " +
 		"where DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 8 hour),'%Y-%m-%d %H') = DATE_FORMAT(date_add(?,interval -1 hour),'%Y-%m-%d %H') " +
-		"and status in (70,60)" +
-		"and flow_id not in(" +
-		"select flow_id " +
-		"from azkaban.execution_jobs " +
-		"where DATE_FORMAT(date_add(from_unixtime(end_time/1000,'%Y-%m-%d %H'),interval 8 hour),'%Y-%m-%d %H') = DATE_FORMAT(date_add(?,interval -1 hour),'%Y-%m-%d %H') " +
-		"and status not in (70,60))"
+		"group by flow_id " +
+		")b " +
+		"where a.end_time = b.end_time and a.flow_id = b.flow_id "
 	rows, err := db.Query(sql, currentTime, currentTime)
 
 	if err != nil {
@@ -70,14 +73,14 @@ func (a *AzkabanMotior) Sendnotice() {
 		if executionJobs.status == 70 {
 			message := "\nazkaban调度" + "\nazkaban任务：" + executionJobs.flowJob + "\n任务状态：failed"
 			fmt.Println(message)
-			/*
-				resp, err := http.Get(fmt.Sprintf("http://voice.arch800.com/notice/singleCallByTts?system=Monitoring&errorMsg=%s", url.QueryEscape(message)))
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				fmt.Println(resp)
-			*/
+
+			resp, err := http.Get(fmt.Sprintf("http://voice.arch800.com/notice/singleCallByTts?system=Monitoring&errorMsg=%s", url.QueryEscape(message)))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(resp)
+
 		}
 
 		if executionJobs.status == 60 {
